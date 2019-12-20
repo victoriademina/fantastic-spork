a3bb150fca37d2efbe927cc71e154e490c619af8
Victoria Demina <viktoriia.demina@gmail.com>
2019-12-20 22:21:58 +0000

    Add experience to the fight emulation.

--- src/fantastic_spork/engine.py
+++ src/fantastic_spork/engine.py
@@ -1,8 +1,16 @@
 import math
 
 
-def fight(health_x, health_y):
-    new_health_x = health_x - int(math.ceil(health_y / 2))
-    new_health_y = health_y - int(math.ceil(health_x / 2))
+def fight(health_x, exp_x, health_y, exp_y):
+    strength_x = int(math.ceil((health_x / 2) * ((exp_x + 100) / 100)))
+    strength_y = int(math.ceil((health_y / 2) * ((exp_y + 100) / 100)))
+    new_health_x = health_x - strength_y
+    new_health_y = health_y - strength_x
+    new_exp_x = exp_x + int(math.ceil(strength_y / 2))
+    if new_exp_x > 100:
+        new_exp_x = 100
+    new_exp_y = exp_y + int(math.ceil(strength_x / 2))
+    if new_exp_y > 100:
+        new_exp_y = 100
 
-    return new_health_x, new_health_y
+    return new_health_x, new_exp_x, new_health_y, new_exp_y
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -3,12 +3,13 @@ from fantastic_spork import engine
 
 
 @pytest.mark.parametrize(
-    "health_x,health_y,expected_health_x,expected_health_y",
+    "health_x,exp_x,health_y,exp_y,expected_health_x,expected_exp_x,expected_health_y,expected_exp_y",
     [
-        (100, 100, 50, 50),
-        (75, 100, 25, 62),
-        (10, 1, 9, -4),
+        (100, 0, 100, 0, 50, 25, 50, 25),
+        (100, 100, 100, 0, 50, 100, 0, 50),
+        (10, 45, 1, 99, 9, 46, -7, 100),
     ],
 )
-def test_fight(health_x, health_y, expected_health_x, expected_health_y):
-    assert engine.fight(health_x, health_y) == (expected_health_x, expected_health_y)
+def test_fight(health_x, exp_x, health_y, exp_y, expected_health_x, expected_exp_x, expected_health_y, expected_exp_y):
+    assert engine.fight(health_x, exp_x, health_y, exp_y) == (
+        expected_health_x, expected_exp_x, expected_health_y, expected_exp_y)
