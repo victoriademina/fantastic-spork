d173ba6511930cb9d890c9aa8c61f0af655ec2a6
Victoria Demina <viktoriia.demina@gmail.com>
2019-12-22 21:14:55 +0000

    Add class for representing units and rewrite functions to use this class.

--- src/fantastic_spork/engine.py
+++ src/fantastic_spork/engine.py
@@ -1,19 +1,44 @@
 import math
 
 
-def fight(health_x, exp_x, health_y, exp_y):
-    strength_x = int(math.ceil((health_x / 2) * ((exp_x + 100) / 100)))
-    strength_y = int(math.ceil((health_y / 2) * ((exp_y + 100) / 100)))
-    new_health_x = health_x - strength_y
-    new_health_y = health_y - strength_x
-    new_exp_x = exp_x + int(math.ceil(strength_y / 2))
-    if new_exp_x > 100:
-        new_exp_x = 100
-    new_exp_y = exp_y + int(math.ceil(strength_x / 2))
-    if new_exp_y > 100:
-        new_exp_y = 100
+class Unit:
+    def __init__(self, x, y, health, experience):
+        """Constructor"""
+        self.x = x
+        self.y = y
+        self.health = health
+        self.experience = experience
 
-    return new_health_x, new_exp_x, new_health_y, new_exp_y
+    def is_alive(self):
+        """Returns True if the unit is alive"""
+        if self.health > 0:
+            return True
+        else:
+            return False
+
+    def strength(self):
+        """Calculates the unit's strength"""
+        result = int(math.ceil((self.health / 2) * ((self.experience + 100) / 100)))
+        return result
+
+    def wound(self, points):
+        """Updates health and experience after a fight"""
+        self.health = self.health - points
+        self.experience = self.experience + int(math.ceil(points / 2))
+        if self.experience > 100:
+            self.experience = 100
+
+    def set_coordinates(self, x, y):
+        self.x = x
+        self.y = y
+
+
+def fight(unit_x, unit_y):
+    x = unit_x.strength()
+    y = unit_y.strength()
+
+    unit_x.wound(y)
+    unit_y.wound(x)
 
 
 def calc_new_coordinates(landscape, coordinates, direction):
@@ -32,44 +57,36 @@ def calc_new_coordinates(landscape, coordinates, direction):
 
 
 def find_unit(units, coordinates):
-    count = 0
     for i in units:
-
-        if i[0] == coordinates[0] and i[1] == coordinates[1]:
-            return count
-        else:
-            count += 1
+        if i.x == coordinates[0] and i.y == coordinates[1]:
+            return i
     return None
 
 
 def move_unit(landscape, units, coordinates, direction):
-    id = find_unit(units, coordinates)
-    unit = units[id]
+    unit = find_unit(units, coordinates)
+
     new_coordinates = calc_new_coordinates(landscape, coordinates, direction)
     if new_coordinates is None:
         return None
-    opponent_id = find_unit(units, new_coordinates)
-    if opponent_id is None:
-        new_unit = new_coordinates[0], new_coordinates[1], unit[2], unit[3]
-        units[id] = new_unit
+
+    opponent = find_unit(units, new_coordinates)
+    if opponent is None:
+        unit.set_coordinates(new_coordinates[0], new_coordinates[1])
         return "Moved"
-    opponent = units[opponent_id]
-    fight_result = fight(unit[2], unit[3], opponent[2], opponent[3])
-    new_unit = unit[0], unit[1], fight_result[0], fight_result[1]
-    new_opponent = opponent[0], opponent[1], fight_result[2], fight_result[3]
-    if fight_result[2] <= 0 and fight_result[0] > 0:
+
+    fight(unit, opponent)
+
+    if unit.is_alive() and not opponent.is_alive():
         units.remove(opponent)
-        new_new_unit = new_coordinates[0], new_coordinates[1], fight_result[0], fight_result[1]
-        units[id] = new_new_unit
+        unit.set_coordinates(new_coordinates[0], new_coordinates[1])
         return "Win"
-    if fight_result[0] <= 0:
-        if fight_result[2] <= 0:
+
+    if not unit.is_alive():
+        if not opponent.is_alive():
             units.remove(opponent)
-        else:
-            units[opponent_id] = new_opponent
         units.remove(unit)
         return "Loose"
-    if fight_result[2] > 0 and fight_result[0] > 0:
-        units[id] = new_unit
-        units[opponent_id] = new_opponent
+
+    if unit.is_alive() and opponent.is_alive():
         return "Draw"
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -3,16 +3,89 @@ from fantastic_spork import engine
 
 
 @pytest.mark.parametrize(
-    "health_x,exp_x,health_y,exp_y,expected_health_x,expected_exp_x,expected_health_y,expected_exp_y",
+    "health,expected",
     [
-        (100, 0, 100, 0, 50, 25, 50, 25),
-        (100, 100, 100, 0, 50, 100, 0, 50),
-        (10, 45, 1, 99, 9, 46, -7, 100),
+        (100, True),
+        (1, True),
+        (0, False),
+        (-1, False),
+    ]
+)
+def test_unit_is_alive(health, expected):
+    unit = engine.Unit(x=0, y=0, health=health, experience=0)
+    assert unit.is_alive() == expected
+
+
+@pytest.mark.parametrize(
+    "health,experience,expected",
+    [
+        (100, 0, 50),
+        (1, 0, 1),
+        (100, 100, 100),
+    ]
+)
+def test_unit_strength(health, experience, expected):
+    unit = engine.Unit(x=0, y=0, health=health, experience=experience)
+    assert unit.strength() == expected
+
+
+@pytest.mark.parametrize(
+    "health,experience,points,expected_health, expected_experience",
+    [
+        (100, 0, 50, 50, 25),
+        (100, 100, 50, 50, 100),
+    ]
+)
+def test_unit_wound(health, experience, points, expected_health, expected_experience):
+    unit = engine.Unit(x=0, y=0, health=health, experience=experience)
+    unit.wound(points)
+    assert unit.health == expected_health
+    assert unit.experience == expected_experience
+
+
+def test_unit_set_coordinates():
+    new_x = 20
+    new_y = 30
+    unit = engine.Unit(x=0, y=0, health=100, experience=0)
+    unit.set_coordinates(new_x, new_y)
+    assert unit.x == new_x
+    assert unit.y == new_y
+
+
+def test_unit_fight():
+    unit_x = engine.Unit(x=0, y=0, health=100, experience=50)
+    unit_y = engine.Unit(x=1, y=1, health=80, experience=0)
+    engine.fight(unit_x, unit_y)
+    assert unit_x.health == 60
+    assert unit_x.experience == 70
+    assert unit_y.health == 5
+    assert unit_y.experience == 38
+
+
+def test_find_unit():
+    units = [engine.Unit(0, 0, 100, 0), engine.Unit(1, 1, 100, 0)]
+    coordinates = (1, 1)
+    assert engine.find_unit(units, coordinates) == units[1]
+
+
+def test_find_unit_none():
+    units = [engine.Unit(0, 0, 100, 0), engine.Unit(1, 1, 100, 0)]
+    coordinates = (2, 1)
+    assert engine.find_unit(units, coordinates) is None
+
+
+@pytest.mark.parametrize(
+    "landscape,units,coordinates,direction,expected",
+    [
+        ([[1, 1], [1, 1]], [engine.Unit(0, 0, 100, 0), engine.Unit(0, 1, 100, 0)], (0, 0), "E", "Moved"),
+        ([[1, 1], [1, 1]], [engine.Unit(0, 0, 100, 0), engine.Unit(0, 1, 1, 0)], (0, 0), "N", "Win"),
+        ([[1, 1], [1, 1]], [engine.Unit(0, 0, 100, 0), engine.Unit(0, 1, 100, 0)], (0, 0), "N", "Draw"),
+        ([[1, 1], [1, 1]], [engine.Unit(0, 0, 1, 0), engine.Unit(0, 1, 100, 0)], (0, 0), "N", "Loose"),
+        ([[1, 1], [1, 1]], [engine.Unit(0, 0, 100, 0), engine.Unit(0, 1, 100, 0)], (0, 0), "S", None),
     ],
 )
-def test_fight(health_x, exp_x, health_y, exp_y, expected_health_x, expected_exp_x, expected_health_y, expected_exp_y):
-    assert engine.fight(health_x, exp_x, health_y, exp_y) == (
-        expected_health_x, expected_exp_x, expected_health_y, expected_exp_y)
+def test_move_unit(landscape, units, coordinates, direction, expected):
+    assert engine.move_unit(landscape, units, coordinates, direction) == expected
 
 
 @pytest.mark.parametrize(
@@ -25,30 +98,3 @@ def test_fight(health_x, exp_x, health_y, exp_y, expected_health_x, expected_exp
 )
 def test_calc_new_coordinates(landscape, coordinates, direction, expected):
     assert engine.calc_new_coordinates(landscape, coordinates, direction) == expected
-
-
-@pytest.mark.parametrize(
-    "units, coordinates,expected",
-    [
-        ([(0, 0, 100, 0), (3, 4, 50, 25)], (3, 4), 1),
-        ([(0, 0, 100, 0), (3, 4, 50, 25)], (2, 2), None),
-
-    ],
-)
-def test_find_unit(units, coordinates, expected):
-    assert engine.find_unit(units, coordinates) == expected
-
-
-@pytest.mark.parametrize(
-    "landscape,units,coordinates,direction,expected",
-    [
-        ([[1, 1], [1, 1]], [(0, 0, 100, 0), (0, 1, 100, 0)], (0, 0), "E", "Moved"),
-        ([[1, 1], [1, 1]], [(0, 0, 100, 0), (0, 1, 1, 0)], (0, 0), "N", "Win"),
-        ([[1, 1], [1, 1]], [(0, 0, 100, 0), (0, 1, 100, 0)], (0, 0), "N", "Draw"),
-        ([[1, 1], [1, 1]], [(0, 0, 1, 0), (0, 1, 100, 0)], (0, 0), "N", "Loose"),
-        ([[1, 1], [1, 1]], [(0, 0, 100, 0), (0, 1, 100, 0)], (0, 0), "S", None),
-
-    ],
-)
-def test_move_unit(landscape, units, coordinates, direction, expected):
-    assert engine.move_unit(landscape, units, coordinates, direction) == expected
